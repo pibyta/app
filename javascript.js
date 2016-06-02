d6c35c1997e73f3e11253b2ccb09c626d6d9dcfa
pibyta <silvina.orengo@gmail.com>
2016-06-02 14:21:37 -0300

    mejor maquetado

--- javascript.js
+++ javascript.js
@@ -2,20 +2,55 @@
 document.addEventListener("devideReady",arrancar,false);
 
 function arrancar(){
-	//urlabsoluta/funcion
-getJSON("http:localhost/ecommercelistaProductos.php",datos,mostrarProductos);
+	//apenas arranca debe traer las imagenes
+		//urlabsoluta/funcion
+//getJSON("http:www.capricholucero/app/img_personajes.php",datos,mostrarImgPersonajes);
+
+//getJSON("http:www.capricholucero/app/img_lugares.php",datos,mostrarImgLugares);
+
+//getJSON("http:www.capricholucero/app/img_finales.php",datos,mostrarImgFinales);
 }
 
-function mostrarProductos(datos){
-	alert("llegaron los datos");
+
+	
+function mostrarImgPersonajes(){
+//getJSON();
 }
 
-function saludar(){
-alert("hola");
+function mostrarImgLugares(){
+//getJSON();
 }
 
-$("#saludo").click(saludar);
+function mostrarImgFinales(){
+//getJSON();
+}
 
+
+
+
+$("#ponerelnombreid").click(cargar_cuento);
+$("#ponerelnombreid").click(cargar_cuento);
+
+$("#ponerelnombreid").click(llenar_variable);
+
+function llenar_variable(){
+	
+	var personaje="mimosin";
+	alert(personaje);
+		
+}
+
+
+
+
+
+
+function cargar_cuento(){
+
+
+	
+alert("Gracias por seleccionar el personaje de tu cuento, pasaré una variable");
+}
 
 function guardar(){
 	localStorage.setItem("nombre", "Pepe");
@@ -29,10 +64,11 @@ $("#btn_guardar").click(guardar);
 
 $("#btn_ver").click(ver);
 
-
-
-
-
+var array_personajes=new Array();
+array_personajes[0]="http:www.capricholucero/app/img/1.jpg";
+array_personajes[1]="http:www.capricholucero/app/img/2.jpg";
+array_personajes[2]="http:www.capricholucero/app/img/3.jpg";
+array_personajes[3]="http:www.capricholucero/app/img/4.jpg";
 
 function hay(){
 	alert("hay señal");
@@ -41,6 +77,6 @@ function noHay(){
 	alert("no hay señal");
 }
 
-window.addEventListener();
-window.addEventListener('online',hay);
-window.addEventListener('online',noHay);
+//window.addEventListener();
+//window.addEventListener('online',hay);
+//window.addEventListener('online',noHay);
